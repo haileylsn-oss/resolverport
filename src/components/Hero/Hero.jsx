3f882bb575a7601f35a0bd9bed3c1ccfe9714abd
haileylsn-oss <haileylsn@gmail.com>
2025-12-03 16:35:07 +0100

    pp

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,18 +8,19 @@ const Hero = () => {
   return (
     <header className="w-full bg-[#1d2330] border border-[#282e3b] rounded-md overflow-hidden shadow-inner" style={{ boxShadow: "inset 0 -20px 0 0 #262b38" }}>
       <div className="w-full h-[62px] p-0 m-0 flex flex-col items-end justify-between">
-        
-        <div className="w-full h-[40px]">
-          <iframe
-            src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=dark&pref_coin_id=1505&invert_hover=no"
-            width="100%"
-            height="36px"
-            scrolling="auto"
-            frameBorder="0"
-            title="coinlib"
-            className="w-full border-0"
-          ></iframe>
-        </div>
+{/*         
+       <div className="w-full h-[40px]">
+  <iframe
+    src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=dark&pref_coin_ids=1505&default_interval=1h&disable_scroll=true&show_chart=false"
+    width="100%"
+    height="36px"
+    scrolling="auto"
+    frameBorder="0"
+    title="coinlib"
+    className="w-full border-0"
+  ></iframe>
+</div> */}
+
 
         <div className="w-full text-right px-2 py-1 text-[11px] text-[#626b7f] font-normal leading-[14px]">
           <a
